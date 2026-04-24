395760b7bc6760969dfb5f68d7ab7678ba487d93
VladBogach <bogach.vladislav@gmail.com>
2026-04-23 21:26:46 -0300

    unification

--- src/app/utils/getApiUrl.ts
+++ src/app/utils/getApiUrl.ts
@@ -3,11 +3,13 @@ const getApiUrl = (env: string) => {
     case 'dev':
       return 'https://europe-west1-fohn-demo.cloudfunctions.net/v2'
     case 'prod':
-      return 'https://us-central1-fohnjs.cloudfunctions.net/v2'
+      // return 'https://us-central1-fohnjs.cloudfunctions.net/v2'
+      return 'https://europe-west1-fohn-demo.cloudfunctions.net/v2'
     case 'dev-local':
       return 'http://127.0.0.1:5001/fohn-demo/europe-west1/v2'
     case 'prod-local':
-      return 'http://127.0.0.1:5001/fohnjs/europe-west1/v2'
+      // return 'http://127.0.0.1:5001/fohnjs/europe-west1/v2'
+      return 'http://127.0.0.1:5001/fohn-demo/europe-west1/v2'
   }
 }
 
